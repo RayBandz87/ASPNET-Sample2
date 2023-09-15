7e1d7fd56212be690f07869f270148d27fe648f0
Raymond Wannamaker Jr <99148885+RayBandz87@users.noreply.github.com>
2023-09-14 20:47:21 -0400

    Add files via upload

---
+++ Pages/WebForms.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASPNET.Pages
+{
+    public class WebForms : PageModel
+    {
+        private readonly ILogger<WebForms> _logger;
+
+        public WebForms(ILogger<WebForms> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
